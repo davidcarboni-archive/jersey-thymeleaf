d61068e7f356814faa9e8c3f53d829e8c6369143
david <david@carboni.io>
2014-07-22 13:16:11 +0100

    Updated pom and run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #export MONGOLAB_URI=mongodb://localhost/listpoint
 mvn clean package && \
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -jar target/dependency/jetty-runner.jar --port 8080 target/listpoint-rest.war
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -jar target/dependency/jetty-runner.jar --port 8080 target/jersey-thymeleaf.war
