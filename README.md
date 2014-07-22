af0126c957fe840db879203b4b2b9823627c2c68
david <david@carboni.io>
2014-07-22 11:40:09 +0100

    Basics

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Jersey meets Thymeleaf
+-----
+
+		
+David Carboni
+
+[https://github.com/davidcarboni/](https://github.com/davidcarboni/)
+
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#export MONGOLAB_URI=mongodb://localhost/listpoint
+mvn clean package && \
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -jar target/dependency/jetty-runner.jar --port 8080 target/listpoint-rest.war
